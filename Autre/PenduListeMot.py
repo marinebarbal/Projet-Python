1f1187465066ce08640c5d3f1d4634802f6bf2d1
marinebarbal <marine.barbal@gmail.com>
2018-08-25 18:08:40 +0200

    mise a jour Regression Linéaire"

---
+++ Autre/PenduListeMot.py
@@ -0,0 +1,23 @@
+liste_mots = [
+    "armoire",
+    "boucle",
+    "buisson",
+    "bureau",
+    "chaise",
+    "carton",
+    "couteau",
+    "fichier",
+    "garage",
+    "glace",
+    "journal",
+    "kiwi",
+    "lampe",
+    "liste",
+    "montagne",
+    "remise",
+    "sandale",
+    "taxi",
+    "vampire",
+    "volant",
+]
+nbchance=8
